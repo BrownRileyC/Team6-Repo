4f824deea2590e4d9e9b1b32313f33ab898335cc
Riley Brown <brown.riley.c@gmail.com>
2019-03-05 20:09:47 -0500

    Set up the tasks so we can hardcode them in later if we wish

--- routes/eventsRoutes.js
+++ routes/eventsRoutes.js
@@ -29,27 +29,44 @@ module.exports = function (app) {
     // Here I need to find a way to grab the current user's userID so we can properly make the association
     // Possibly we store the userID in localstorage when they log in so we can grab it as part of the post request?
     console.log(req.body);
+    console.log(req.body.userID);
 
     db.Events.create({
       eventName: req.body.eventName,
       eventDate: req.body.eventDate,
       locationName: req.body.locationName,
+      eventType: req.body.eventType,
       score: req.body.score,
       UserId: req.body.userID
     }
       , {
         include: [db.Users, db.Tasks]
       }).then(function (dbEvents) {
-        console.log(dbEvents);
-        console.log(dbEvents.dataValues.id)
-        db.Tasks.bulkCreate([
-          { task: "First Task", EventId: dbEvents.dataValues.id },
-          { task: "Second Task", EventId: dbEvents.dataValues.id },
-          { task: "Third Task", EventId: dbEvents.dataValues.id }
-        ]).then(function (dbTasks) {
-          res.json(dbEvents);
-        });
-
+        if (req.body.eventType === "Interview") {
+          db.Tasks.bulkCreate([
+            { task: "First Interview Task", EventId: dbEvents.dataValues.id },
+            { task: "Second Interview Task", EventId: dbEvents.dataValues.id },
+            { task: "Third Interview Task", EventId: dbEvents.dataValues.id }
+          ]).then(function (dbTasks) {
+            res.redirect("/api/events/"+req.body.userID);
+          });
+        } else if (req.body.eventType === "Networking") {
+          db.Tasks.bulkCreate([
+            { task: "First Networking Task", EventId: dbEvents.dataValues.id },
+            { task: "Second Networking Task", EventId: dbEvents.dataValues.id },
+            { task: "Third Networking Task", EventId: dbEvents.dataValues.id }
+          ]).then(function (dbTasks) {
+            res.redirect("/api/events/"+req.body.userID);
+          });
+        } else {
+          db.Tasks.bulkCreate([
+            { task: "First Presentation Task", EventId: dbEvents.dataValues.id },
+            { task: "Second Presentation Task", EventId: dbEvents.dataValues.id },
+            { task: "Third Presentation Task", EventId: dbEvents.dataValues.id }
+          ]).then(function (dbTasks) {
+            res.redirect("/api/events/"+req.body.userID);
+          });
+        }
       });
   })
   // Delete an example by id
