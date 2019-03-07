00f0fbbcf42eb98acb24430f53bbd4a03021482d
Riley Brown <brown.riley.c@gmail.com>
2019-03-07 18:06:16 -0500

    Still not working

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,11 +3,11 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-      res.sendFile("/index.html")
+      res.sendFile("./index.html")
   });
 
   app.get("/event", function(req, res) {
-    res.sendFile("/event.html")
+    res.sendFile("./event.html")
 });
   // // Load example page and pass in an example by id
   // app.get("/example/:id", function(req, res) {
