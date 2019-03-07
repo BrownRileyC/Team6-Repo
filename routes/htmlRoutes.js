20aaf167f69ab3167226e61aecc19c6475c60ceb
Riley Brown <brown.riley.c@gmail.com>
2019-03-07 18:33:02 -0500

    another attempt

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,14 +1,14 @@
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Load index page
-  app.get("/", function(req, res) {
-      res.sendFile(path.join(__dirname, "./public/index.html"))
+  app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"))
   });
 
-  app.get("/event", function(req, res) {
-    res.sendFile(path.join(__dirname, "./public/event.html"))
-});
+  app.get("/event", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/event.html"))
+  });
   // // Load example page and pass in an example by id
   // app.get("/example/:id", function(req, res) {
   //   db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
@@ -19,7 +19,7 @@ module.exports = function(app) {
   // });
 
   // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
+  app.get("*", function (req, res) {
     res.render("404");
   });
 };
